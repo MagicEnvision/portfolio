0b89b31e8324b0aa3566b1cae711079e833401c9
magicenvision <rjrose2003@gmail.com>
2025-01-23 23:23:39 -0500

    added cards

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,12 +2,24 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='text-white mt-[650px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore a explicabo culpa! Voluptatem nihil, itaque nam ducimus maxime incidunt? Enim totam quas porro harum at excepturi aspernatur dolor rerum vitae!
-    Assumenda dignissimos cum praesentium. Delectus itaque veritatis provident, quos exercitationem aut. Repellendus, deserunt nihil quo saepe laudantium temporibus, officia tempora nesciunt animi molestias eveniet, eaque omnis? Earum quaerat repellendus esse.
-    Autem quam perspiciatis ea qui odio animi tempore suscipit rem placeat sequi officia id, optio quia necessitatibus maxime! Soluta minus at eum iusto suscipit veritatis quam amet alias, asperiores quas!
-    Neque adipisci sed inventore iusto hic aperiam qui, est magni perferendis commodi ad at harum, fuga consectetur aliquid ipsum. Quo iusto ipsa repellendus quidem fugit blanditiis aut ducimus natus. Voluptates.
-    At impedit nam voluptate similique repellendus, obcaecati temporibus. Eaque, illo dolorem aperiam nostrum doloribus dignissimos alias architecto amet rerum mollitia laudantium quia at. Esse dolores atque repellat magnam quos assumenda.</div>
-  )
+    <>
+    <div className='flex text-center justify-center md:flex-row flex-col mr-22 text-white flex-1 w-full'>
+
+      <div className='border-2 flex-grow rounded-tl-3xl rounded-br-3xl rounded-bl-sm rounded-tr-md border-neutral-800 bg-neutral-900 p-5 m-5'>
+      <h1 className='text-white'>My Favourite Tech Stack</h1>
+      <img src="" alt="" />
+      <p>MERN </p>
+      </div>
+     
+      <div className='border-2 flex-grow rounded-tl-3xl rounded-br-3xl rounded-bl-sm rounded-tr-md border-neutral-800 bg-neutral-900 p-5 m-5'>
+      <h1 className='text-white'>College Degree</h1>
+      <img src="" alt="" />
+      <p className='pt-1'>George Brown College<br></br>Programmer/Analyst</p>
+      </div>
+
+    </div>
+    </>
+    )
 }
 
 export default Hero
