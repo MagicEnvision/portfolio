7009b741075e6371b08d41e16fb8d4c2e8a85de6
magicenvision <rjrose2003@gmail.com>
2025-04-14 16:41:06 -0400

    added cards

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,48 +1,43 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 const NavBar = () => {
-    const [menuToggle, setMenuToggle] = useState(false)
+  const [menuToggle, setMenuToggle] = useState(false);
 
-    const handleToggleChange = () => {
-        setMenuToggle(!menuToggle)
-    }
-    return (
-        <div >
-            <div className='border-[2px] bg-neutral-900 border-neutral-800 w-full flex h-[42px] mt-4 rounded-tl-3xl rounded-bl-md rounded-tr-md rounded-br-3xl  '>
-                <div className="container flex  justify-between items-center px-6">
-                    
-                    <h1 className='text-white'>Rj Rose</h1>
-                    
-                <div className={`cursor-pointer md:hidden  transform duration-1000 ${!menuToggle ? "rotate-90" : "rotate-180"}`} onClick={handleToggleChange} >
-                    <span className='m-[2px] border-2 border-neutral-500 w-8 h-2 rounded-tl-lg rounded-tr-sm rounded-bl-sm rounded-br-lg'></span>
-                    <span className='m-[2px] border-2 border-neutral-500 w-8 h-2 rounded-tl-lg rounded-tr-sm rounded-bl-sm rounded-br-lg'></span>
-                    <span className='m-[2px] border-2 border-neutral-500 w-8 h-2 rounded-tl-lg rounded-tr-sm rounded-bl-sm rounded-br-lg'></span>
-                    </div>
-                    <ul className={`text-white md:flex ${!menuToggle ? "hidden" : "absolute top-[4rem] flex flex-col gap-3 right-5"}`}>
-                        <li className='bg-neutral-900
+  const handleToggleChange = () => {
+    setMenuToggle(!menuToggle);
+  };
+  return (
+    <div>
+      <div className="border-[2px] bg-neutral-900 border-neutral-800 w-full flex h-[42px] mt-4 rounded-tl-3xl rounded-bl-md rounded-tr-md rounded-br-3xl  ">
+        <div className="container flex  justify-center items-center px-6">
+          <h1 className="text-white font-bold tracking-widest">Web Development Portfolio <span className="-tracking-tighter font-thin">by Rj Rose</span></h1>
+
+          {/* <div
+            className={`cursor-pointer md:hidden  transform duration-1000 ${
+              !menuToggle ? "rotate-90" : "rotate-180"
+            }`}
+            onClick={handleToggleChange}
+          >
+            <span className="m-[2px] border-2 border-neutral-500 w-8 h-2 rounded-tl-lg rounded-tr-sm rounded-bl-sm rounded-br-lg"></span>
+            <span className="m-[2px] border-2 border-neutral-500 w-8 h-2 rounded-tl-lg rounded-tr-sm rounded-bl-sm rounded-br-lg"></span>
+            <span className="m-[2px] border-2 border-neutral-500 w-8 h-2 rounded-tl-lg rounded-tr-sm rounded-bl-sm rounded-br-lg"></span>
+          </div>
+          <ul
+            className={`text-white md:flex ${
+              !menuToggle
+                ? "hidden"
+                : "absolute top-[4rem] flex flex-col gap-3 right-5"
+            }`}
+          >
+            <li className='bg-neutral-900
              border-neutral-800 border-[2px] h-[30px] rounded-tl-3xl 
               rounded-bl-lg rounded-tr-lg rounded-br-3xl hover:shadow
-            hover:shadow-neutral-400 flex items-center justify-center px-[12px] mx-4 '>Social</li>
-                        <li className='bg-neutral-900
-             border-neutral-800 border-[2px]  h-[30px] rounded-tl-3xl 
-              rounded-bl-lg rounded-tr-lg rounded-br-3xl hover:shadow
-            hover:shadow-neutral-400 flex items-center justify-center px-[12px] mx-4 '>Projects</li>
-                        <li className='bg-neutral-900
-             border-neutral-800 border-[2px] h-[30px] rounded-tl-3xl 
-              rounded-bl-lg rounded-tr-lg rounded-br-3xl hover:shadow
-            hover:shadow-neutral-400 flex items-center justify-center px-[12px] mx-4 '>Resume</li>
-                        <li className='bg-neutral-900
-             border-neutral-800 border-[2px] h-[30px] rounded-tl-3xl 
-              rounded-bl-lg rounded-tr-lg rounded-br-3xl hover:shadow
-            hover:shadow-neutral-400 flex items-center justify-center px-[12px] mx-4'>About Me</li>
-                    </ul>
-
-                </div>
-            </div>
-          
-
+            hover:shadow-neutral-400 flex items-center justify-center px-[12px] mx-4 '>Social</li> 
+          </ul> */}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,49 +1,57 @@
-import React from 'react'
-import * as THREE from 'three';
-
 const Hero = () => {
-
-  const loader = new THREE.ObjectLoader();
-
-  loader.load(
-      // resource URL
-      "models/json/example.json",
-
-      // onLoad callback
-      // Here the loaded data is assumed to be an object
-      function ( obj ) {
-        // Add the loaded object to the scene
-        scene.add( obj );
-      },
-
-      // onProgress callback
-      function ( xhr ) {
-        console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-      },
-
-      // onError callback
-      function ( err ) {
-        console.error( 'An error happened' );
-      }
-  );
-
-
-// Alternatively, to parse a previously loaded JSON structure
-  const object = loader.parse( a_json_object );
-
-  scene.add( object );
-
   return (
     <>
-    <div className='container w-full p-5 '>
-      <div className=''>
+     <section className="w-full">
+     <div className="flex gap-5 flex-col sm:flex-row">
+        <div
+          className="border-[2px] mt-6 m-auto bg-neutral-900 border-neutral-800 w-[50%] flex h-[400px] 
+        rounded-tl-3xl rounded-bl-md rounded-tr-md rounded-br-3xl"
+        >
+          <div
+            className="border-neutral-800 border-b-2 rounded-tl-3xl 
+  rounded-tr-md bg-neutral-900 p-4 w-full flex flex-col :"
+          >
+            <h1 className="text-white text-3xl tracking-tight font-bold">
+              Website to View Gaming Leaderboards
+            </h1>
+            <p className="text-white text-xl">
+              Using three.js, gsap.js, react.
+            </p>
+            <span className="border-neutral-800 border-b-2 w-full pt-3"></span>
+          </div>
 
+          
+        </div>
+
+
+        <div
+          className="border-[2px] mt-6 m-auto bg-neutral-900 border-neutral-800 w-[50%] flex h-[400px] 
+        rounded-tl-3xl rounded-bl-md rounded-tr-md rounded-br-3xl"
+        >
+          <div
+            className="border-neutral-800 border-b-2 rounded-tl-3xl 
+  rounded-tr-md bg-neutral-900 p-4 w-full flex flex-col "
+          >
+            <h1 className="text-white text-3xl tracking-tight font-bold">
+              Website to View Gaming Leaderboards
+            </h1>
+            <p className="text-white text-xl">
+              Using three.js, gsap.js, react.
+            </p>
+            <span className="border-neutral-800 border-b-2 w-full pt-3"></span>
+          </div>
+
+          
+
+        </div>
+        
+        
 
       </div>
-
-    </div>
+      
+     </section>
     </>
-    )
-}
+  );
+};
 
-export default Hero
+export default Hero;
