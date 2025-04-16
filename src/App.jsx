ac359cc0391c0d8b57dc42a27a7f3155e306ca96
magicenvision <rjrose2003@gmail.com>
2025-04-16 11:42:25 -0400

    added second layer

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
     <>
-   <div className='flex flex-start h-[1000px] justify-center bg-neutral-950 px-4'>
-    <div className='container'>
+   <div className='flex flex-start h-full justify-center bg-neutral-950 pb-[1000px] px-4'>
+    <div className=''>
     <NavBar /> 
     <Hero /> 
     </div>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,78 +1,70 @@
-
-
-
 const Hero = () => {
   const handleMouseMove = (e) => {
     e.currentTarget.play()
   }
-  
+
   const handleMouseLeave = (e) => {
     e.currentTarget.pause()
   }
 
   return (
-    <>
-     <section className="w-full">
-     <div className="flex gap-5 flex-col w-full justify-center mt-5 flex-grow sm:flex-row">
-      
-     <div className="border-[2px]  md:w-[50vw] bg-neutral-900 border-neutral-800 md:hover:translate-x-[-5%] md:hover:translate-y-[5%]  transition ease-in-out m-auto delay-250 flex  h-[500px] 
-        rounded-tl-3xl rounded-bl-md rounded-tr-md rounded-br-3xl">
+    <section className="w-full ">
+      <div className="flex gap-5 flex-wrap sm:flex-nowrap justify-center">
+        {[
+          { title: "Awwards Winning Website", src: "/1.mkv", subTitle: "Using three.js, gsap.js, react." },
+          { title: "Apple iPhone Website Clone", src: "/2.mkv", subTitle: "Using three.js, gsap.js, react."}
+        ].map((card, idx) => (
+          <div
+            key={idx}
+            className="w-full md:w-1/2 border-2 bg-neutral-900 border-neutral-800 hover:scale-95 hover:z-10 transform transition duration-300 ease-in-out h-[500px] rounded-tl-3xl rounded-bl-md rounded-tr-md rounded-br-3xl"
+          >
+            <div className="p-4 w-full h-full flex flex-col overflow-hidden">
+              <h1 className="text-white text-3xl tracking-tight font-bold">
+                {card.title}
+              </h1>
+              <p className="text-white text-xl">{card.subTitle}</p>
+              <span className="border-neutral-800 border-b-2 w-full my-5" />
 
-          <div className="p-4 w-full flex flex-col overflow-hidden">
-            <h1 className="text-white text-3xl tracking-tight font-bold">
-              Awwards Winning Website</h1>
-
-            <p className="text-white text-xl">
-              Using three.js, gsap.js, react.</p>
-            <span className="border-neutral-800 border-b-2 w-full my-5 "/>
-  
-            <video
-            className="rounded-tl-3xl rounded-bl-md rounded-tr-md rounded-br-3xl border-neutral-800 border-[2px]" 
-            src="/1.mkv"
-            onMouseMove={handleMouseMove}
-            onMouseLeave={handleMouseLeave}
-            muted/>
-
-
-
-       
+              <video
+                className="rounded-tl-3xl rounded-bl-md rounded-tr-md rounded-br-3xl border-neutral-800 border-[2px] w-full h-full object-cover"
+                src={card.src}
+                onMouseMove={handleMouseMove}
+                onMouseLeave={handleMouseLeave}
+                muted
+              />
+            </div>
           </div>
-        </div>
-
-        <div className="border-[2px]  md:w-[50vw] bg-neutral-900 border-neutral-800  flex h-[500px] 
-        rounded-tl-3xl rounded-bl-md rounded-tr-md rounded-br-3xl">
-
-          <div className="p-4 w-full flex flex-col overflow-hidden">
-            <h1 className="text-white text-3xl tracking-tight font-bold">
-              Apple iPhone Website Clone </h1>
-
-            <p className="text-white text-xl">
-              Using three.js, gsap.js, react.</p>
-            <span className="border-neutral-800 border-b-2 w-full my-5 "/>
-  
-            <video
-            className="rounded-tl-3xl rounded-bl-md rounded-tr-md rounded-br-3xl border-neutral-800 border-[2px]" 
-            src="/2.mkv" 
-            onMouseMove={handleMouseMove}
-            onMouseLeave={handleMouseLeave}
-            muted/>
-
-       
-
-
-
-       
-          </div>
-        </div>
-
-
-
-        
-
+        ))}
       </div>
-      
-     </section>
-    </>
+      <div className="flex gap-5 mt-6 justify-center">
+        {[
+          { title: "Awwards Winning Website", src: "/1.mkv", subTitle: "Using three.js, gsap.js, react." },
+          { title: "Apple iPhone Website Clone", src: "/2.mkv", subTitle: "Using three.js, gsap.js, react."}
+        ].map((card, idx) => (
+          <div
+            key={idx}
+            className="w-full md:w-1/2 border-2 bg-neutral-900 border-neutral-800 hover:scale-95 hover:z-10 transform transition duration-300 ease-in-out h-[500px] rounded-tl-3xl rounded-bl-md rounded-tr-md rounded-br-3xl"
+          >
+            <div className="p-4 w-full h-full flex flex-col overflow-hidden">
+              <h1 className="text-white text-3xl tracking-tight font-bold">
+                {card.title}
+              </h1>
+              <p className="text-white text-xl">{card.subTitle}</p>
+              <span className="border-neutral-800 border-b-2 w-full my-5" />
+
+              <video
+                className="rounded-tl-3xl rounded-bl-md rounded-tr-md rounded-br-3xl border-neutral-800 border-[2px] w-full h-full object-cover"
+                src={card.src}
+                onMouseMove={handleMouseMove}
+                onMouseLeave={handleMouseLeave}
+                muted
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+    
   );
 };
 
