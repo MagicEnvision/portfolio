1870c440eacda2900a53085e735e56ac7c4477f9
magicenvision <rjrose2003@gmail.com>
2025-08-12 21:46:25 -0400

    added useState navigation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import Hero from './sections/Hero'
 import ModelView from './components/ModelView'
 import React from 'react'
 import { useState, useEffect } from 'react'
+import WhatIDo from './components/WhatIDo'
+import Contact from './components/Contact'
+import Projects from './components/Projects'
+import SideHobby from './components/SideHobby'
+
+
 function App() {
   const [isMobile, setIsMobile] = useState(false)
 
@@ -14,6 +20,8 @@ function App() {
   //   return () => window.removeEventListener('resize', handleResize);
   //   }
   // }, [isMobile])
+  const  [activePage, setActivePage] = useState("me")
+
 
   return (
     <>
@@ -21,18 +29,20 @@ function App() {
     
     <div className='hidden sm:flex sm:w-[20vw] bg-gradient-to-b from-neutral-600 to-neutral-800 justify-center rounded-[2rem] p-[1px] relative my-4'>
     <ul className='flex flex-col justify-center box-border w-full items-center bg-neutral-950 p-4 rounded-[2rem]'>
-      <li className=''><p>hellow</p></li>
-       <li><p>hellow</p></li>
-        <li><p>hellow</p></li>
-         <li><p>hellow</p></li>
-          <li><p>hellow</p></li>
-      
+       <li className="cursor-pointer " onClick={() => setActivePage("me")}>What I Do</li>
+       <li className="cursor-pointer " onClick={() => setActivePage("projects")}><p>Projects</p></li>
+       <li className="cursor-pointer " onClick={() => setActivePage("contact")}><p>Contact Me</p></li>
+       <li className="cursor-pointer " onClick={() => setActivePage("sidehobby")}><p>Side Hobbies</p></li>
     </ul>
     </div>
 
-    <div className='flex w-full h-screen sm:h-auto sm:w-[80vw] bg-gradient-to-b from-neutral-600 to-neutral-800 justify-center rounded-[2rem] p-[1px]  sm:my-4 sm:mr-6'>
+    <div className='flex w-full h-screen sm:h-auto sm:w-[80vw] bg-gradient-to-b
+     from-neutral-600 to-neutral-800 justify-center rounded-[2rem] p-[1px] sm:my-4 '>
     <div className='w-full h-full bg-neutral-950 rounded-[2rem] p-5'>
-     {`section 2 `}
+     { activePage === "me" && <WhatIDo /> }
+     { activePage === "projects" && <Projects /> }
+     { activePage === "contact" && <Contact /> }
+     { activePage === "sidehobby" && <SideHobby /> }
     </div>
     </div>
    
---
+++ src/components/SideHobby.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideHobby = () => {
+  return (
+    <div>
+      Side Hobbies
+    </div>
+  )
+}
+
+export default SideHobby
