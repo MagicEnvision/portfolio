5749bebf2892982d8772c0e78d2055eb4f9ced7b
magicenvision <rjrose2003@gmail.com>
2025-08-25 21:30:12 -0400

    added line

--- src/components/WhatIDo.jsx
+++ src/components/WhatIDo.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 const WhatIDo = () => {
   return (
-    <div className='flex flex-nowrap gap-10 w-full border-[1px] bg-[#060606] p-8 overflow-hidden rounded-[2rem] border-neutral-700 '>
-      <div className='rounded-full border-2 flex-none bg-white h-[250px] overflow-hidden w-[250px]'>
+    <div className='flex flex-nowrap items-center gap-10 w-full border-[1px] bg-[#060606] p-8 overflow-hidden rounded-[2rem] border-neutral-700 '>
+      <div className='rounded-full border-2 flex-none bg-white h-[200px] overflow-hidden w-[200px]'>
       <img src="https://placehold.co/250x250" alt="" />
       </div>
-      <div className=' h-[250px] flex-grow-[0] p-1'>
+      <div className='h-[250px] flex-grow-[0]  p-1'>
         <h1 className='text-xl flex justify-between'>
-          <span className='text-2xl font-extrabold font-'>Hi, The Names Rj</span>
-          <span className=''>FullStack Web Enthusiast</span>
+          <span className='text-2xl font-extrabold '>Hi, My Name is Rj </span>
+          <span className='text-2xl font-bold italic'>Full Stack Web Enthusiast</span>
         </h1>
-        <p className='text-center py-4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+       <div className='flex flex-col justify-center items-center  '>
+         <div className='w-full border-t-[1px] my-2'/>
+        <p className='text-center '>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
            Corrupti eveniet dignissimos veniam autem, quia illum eaque. 
            Soluta accusamus fugiat veniam molestiae, impedit reiciendis
             quaerat, nemo perferendis  Distinctio voluptas molestias 
@@ -19,6 +21,7 @@ const WhatIDo = () => {
             ducimus quasi eligendi numquam tempora incidunt assumenda 
             repudiandae possimus voluptatem facilis natus. Totam inventore 
             fugiat aperiam quis dolorem, tempore nostrum natus?</p>
+       </div>
       </div>
     
     </div>
