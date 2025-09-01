d7b120a87bb7a8861eff2b383e5a66ba09469b92
magicenvision <rjrose2003@gmail.com>
2025-09-01 10:33:19 -0400

    working on responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState , useEffect} from 'react';
+import React, { useState , useEffect, act} from 'react';
 import WhatIDo from './components/WhatIDo';
 import Contact from './components/Contact';
 import Projects from './components/Projects';
@@ -6,6 +6,12 @@ import Magicenvision from './components/Magicenvision';
 import './index.css';
 import { div } from 'motion/react-client';
 import { IoMenu } from "react-icons/io5";
+import { HiMiniHome } from "react-icons/hi2";
+import { FaFolderClosed } from "react-icons/fa6";
+import { FaFolderOpen } from "react-icons/fa6";
+import { IoMdContact } from "react-icons/io";
+
+
 
 function App() {
   const [activePage, setActivePage] = useState("me");
@@ -34,42 +40,62 @@ function App() {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-[#060606] p-1">
-      <div className="flex  sm:flex-row w-full max-w-xs md:max-w-2xl lg:max-w-[90rem] xl:max-w-[100rem] h-full max-h-[95vh] sm:h-auto text-white sm:gap-5">
-        <div className= {`flex ${isMobile ? 'w-[10vw] mr-4' : 'w-[20vw]'}  border-[1px] border-neutral-700 justify-center rounded-[2rem] overflow-hidden my-4`}>
+      <div className="flex  sm:flex-row w-full max-w-xs md:max-w-3xl lg:max-w-[90rem] xl:max-w-[100rem] h-full max-h-[95vh] sm:h-auto text-white sm:gap-5">
+        <div className= {`flex ${isMobile ? 'w-[15vw] mr-4' : 'w-[20vw]'}  border-[1px] border-neutral-700 justify-center rounded-[2rem] overflow-hidden my-4`}>
           <ul className="flex flex-col justify-between box-border w-full items-center bg-neutral-950  rounded-[2rem]">
-           {
-            isMobile ? (
-            
-              <button 
-              className='h-full flex flex-col justify-center items-center'
-              onClick={() => {}}>
-              <IoMenu 
-              className='rotate-90 text-neutral-400 cursor-pointer'
-              width={10}
-              height={10}
-              />
+
+            <div className='w-full flex flex-col flex-grow'>
+            <li className={`flex justify-center items-center font-bold font-oswald tracking-tighter border-b-[1px] border-neutral-700 text-2xl w-full text-center p-0 sm:p-5 h-full cursor-pointer 
+             ${activePage === "me" ? 'bg-white text-3xl text-black transition-none' : ''}  `} onClick={() => setActivePage("me")}>
+
+              {isMobile ? 
+              (<HiMiniHome size={24}/>)
+                
+              :
+              (<p className='transition duration-300 ease-in-out delay-110 hover:scale-125'>What I Do</p>)
+    
+              }
+
+            </li>
+            <li className={`flex justify-center items-center font-bold font-oswald tracking-tighter border-b-[1px] border-neutral-700 text-2xl w-full text-center p-0 sm:p-5 h-full cursor-pointer 
+             ${activePage === "projects" ? 'bg-white text-3xl text-black transition-none' : ''} `} onClick={() => setActivePage("projects")}>
+
+              {isMobile ? 
+              (activePage === "projects" ? <FaFolderOpen /> : <FaFolderClosed />)
               
-              </button>
-            ) 
-            : (
-              <div className='w-full flex flex-col flex-grow'>
-            <li className={`flex justify-center items-center font-bold font-oswald tracking-tighter border-b-[1px] border-neutral-700 text-2xl w-full text-center p-5 h-full cursor-pointer 
-             ${activePage === "me" ? 'bg-white text-3xl text-black transition-none' : ''}  `} onClick={() => setActivePage("me")}><p className='transition duration-300 ease-in-out delay-110 hover:scale-125'>What I Do</p></li>
-            <li className={`flex justify-center items-center font-bold font-oswald tracking-tighter border-b-[1px] border-neutral-700 text-2xl w-full text-center p-5 h-full cursor-pointer 
-             ${activePage === "projects" ? 'bg-white text-3xl text-black transition-none' : ''} `} onClick={() => setActivePage("projects")}><p className='transition duration-300 ease-in-out delay-110 hover:scale-125'>Projects</p></li>
-            <li className={`flex justify-center items-center font-bold font-oswald tracking-tighter border-b-[1px] border-neutral-700 text-2xl w-full text-center p-5 h-full cursor-pointer 
-             ${activePage === "contact" ? 'bg-white text-3xl text-black transition-none' : ''} `} onClick={() => setActivePage("contact")}><p className='transition duration-300 ease-in-out delay-110 hover:scale-125'>Contact Me</p></li>
-            <li className={`flex justify-center items-center font-bold font-oswald tracking-tighter border-b-[1px] border-neutral-700 text-2xl w-full text-center p-5 h-full cursor-pointer 
-             ${activePage === "magic" ? 'bg-white text-3xl text-black transition-none' : ''} `} onClick={() => setActivePage("magic")}><p className='transition duration-300 ease-in-out delay-110 hover:scale-125'>Clothing <br /> Brand</p></li> 
-              </div>
-            )
-            
-           }
+              :
+              (<p className='transition duration-300 ease-in-out delay-110 hover:scale-125'>Projects</p>)
+    
+              }
+            </li>
+            <li className={`flex justify-center items-center font-bold font-oswald tracking-tighter border-b-[1px] border-neutral-700 text-2xl w-full text-center p-0 sm:p-5 h-full cursor-pointer 
+             ${activePage === "contact" ? 'bg-white text-3xl text-black transition-none' : ''} `} onClick={() => setActivePage("contact")}>
+
+               {isMobile ? 
+              (<IoMdContact size={24}/>)
+                
+              :
+              (<p className='transition duration-300 ease-in-out delay-110 hover:scale-125'>Contact</p>)
+                }
+            </li>
+              
+            <li className={`flex justify-center items-center font-bold font-oswald tracking-tighter border-b-[1px] border-neutral-700 text-2xl w-full text-center p-0 sm:p-5 h-full cursor-pointer 
+             ${activePage === "magic" ? 'bg-white  text-black transition-none' : ''} `} onClick={() => setActivePage("magic")}>
+
+              {isMobile ? 
+              (activePage === "magic" ? <img src="/finalLightningBoltLogo.svg" size={24} /> : <img src="/whitebolts.svg" size={24} /> )
+                
+              :
+              (<p className='transition duration-300 ease-in-out delay-110 hover:scale-125'>Clothing Brand</p>)
+                }
+            </li> 
+            </div>
+           
 
           </ul>
         </div>
         <div className="flex w-full border-[1px] bg-neutral-950 border-neutral-700 justify-center rounded-[2rem] my-4 overflow-y-auto">
-          <div className="w-full h-screen  rounded-[2rem] flex flex-col items-center p-6">
+          <div className="w-full h-screen  rounded-[2rem] p-4 sm:p-6">
             {activePage === "me" && <WhatIDo />}
             {activePage === "projects" && <Projects />}
             {activePage === "contact" && <Contact />}
--- src/components/Magicenvision.jsx
+++ src/components/Magicenvision.jsx
@@ -1,54 +1,94 @@
 import React from "react";
 import ModelView from "./ModelView.jsx";
-import { motion } from "motion/react"
+import { motion } from "motion/react";
 const MagicEnvision = () => {
   return (
-    <div className="flex flex-col sm:grid sm:grid-cols-2 lg:grid-cols-3 sm:grid-rows-5 gap-6 py-5 mx-6 mb-6">
+    <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 grid-rows-10 gap-4 sm:gap-6 py-5  mb-6">
       <ModelView
-        className={`sm:m-0 border-[1px] rounded-[2rem] border-neutral-700
-           hover:shadow-[0px_0px_5px_0px_#EDEDED] transition-all duration-300 bg-[#060606] col-span-2 row-span-2`}
-        model={'magic'}
+        className={` w-full border-[1px] rounded-[2rem] border-neutral-700
+           hover:shadow-[0px_0px_5px_0px_#EDEDED] transition-all duration-300 bg-[#060606] col-span-1 row-span-1 md:col-span-2 md:row-span-2`}
+        model={"magic"}
         minPolarAngle={2}
         maxPolarAngle={1.8}
       />
+
       <div
         className="border-[1px] rounded-[2rem] flex justify-center items-center flex-col border-neutral-700 
-       hover:shadow-[0px_0px_5px_0px_#EDEDED] transition-all duration-300 w-full pb-1 px-1 sm:py-2 sm:px-2  bg-[#060606] col-span-1 row-span-1"
-      >
-        <h1 className="text-lg tracking-tight text-center font-oswald font-extrabold italic sm:text-xl">
+       hover:shadow-[0px_0px_5px_0px_#EDEDED] transition-all duration-300 w-full bg-[#060606] col-span-1 row-span-1 p-4">
+
+        <h1 className="text-md sm:text-2xl tracking-tight text-center font-oswald font-extrabold italic ">
           Magic Envision{" "}
-          <span className="text-md tracking-tight sm:text-xl font-light">
+          <span className="text-sm sm:text-xl tracking-tight font-light">
             {""}Clothing Brand
           </span>
           <br />
         </h1>
         <div className="border-t-[1px] border-neutral-600 my-2 w-full" />
-          <p className="text-lg leading-6 font-extralight text-center tracking-tight px-2">
-            {" "}
-            In my sparetime I love to create design, create, and manufacture my
-            own clothing. Started my sewing journey in 2020 and Ive loved it
-            {" "}
-          </p>
+        <p className="text-md sm:text-lg leading-4 md:leading-6 font-extralight text-center tracking-tight">
+          {" "}
+          In my sparetime I love to create design, create, and manufacture my
+          own clothing. Started my sewing journey in 2020 and Ive loved it{" "}
+        </p>
       </div>
+
       <div
         className="flex justify-center flex-col border-[1px] rounded-[2rem] bg-[#060606]  border-neutral-700 
        hover:shadow-[0px_0px_5px_0px_#EDEDED] transition-all duration-300 w-full"
       >
-        <ModelView 
-        className={`sm:m-0  p-2 col-span-1 row-span-1`} 
-        model={'bolts'} 
-        minPolarAngle={2}
-        maxPolarAngle={1.5}/>
+        <ModelView
+          className={`  p-2 col-span-1 row-span-1`}
+          model={"bolts"}
+          minPolarAngle={2}
+          maxPolarAngle={1.5}
+        />
       </div>
 
       <div
         className="border-[1px] rounded-[2rem] flex justify-center items-center flex-col border-neutral-700 
-       hover:shadow-[0px_0px_5px_0px_#EDEDED] transition-all duration-300 w-full py-1 px-1 sm:py-2 sm:px-2  col-span-1 row-span-2 bg-[#060606] "
+       hover:shadow-[0px_0px_5px_0px_#EDEDED] transition-all duration-300 w-full col-span-2 row-span-1 lg:col-span-1 bg-[#060606] "
       >
         <h1 className="text-lg  text-center font-oswald font-extrabold italic sm:text-xl ">
-          PlaceHolder
+          1
         </h1>
       </div>
+
+      <div
+        className="border-[1px] rounded-[2rem] flex justify-center items-center flex-col border-neutral-700 
+       hover:shadow-[0px_0px_5px_0px_#EDEDED] transition-all duration-300 w-full  col-span-1 row-span-1 lg:col-span-2 bg-[#060606] "
+      >
+        <h1 className="text-lg  text-center font-oswald font-extrabold italic sm:text-xl ">
+          2
+        </h1>
+      </div>
+
+      <div
+        className="border-[1px] rounded-[2rem] flex justify-center items-center flex-col border-neutral-700 
+       hover:shadow-[0px_0px_5px_0px_#EDEDED] transition-all duration-300 w-full   col-span-1 row-span-1 lg:col-span-2 bg-[#060606] "
+      >
+        <h1 className="text-lg  text-center font-oswald font-extrabold italic sm:text-xl ">
+          3
+        </h1>
+      </div>
+
+       <div
+        className="border-[1px] rounded-[2rem] flex justify-center items-center flex-col border-neutral-700 
+       hover:shadow-[0px_0px_5px_0px_#EDEDED] transition-all duration-300 w-full   col-span-1 row-span-1  bg-[#060606] "
+      >
+        <h1 className="text-lg  text-center font-oswald font-extrabold italic sm:text-xl ">
+          4
+        </h1>
+      </div>
+
+      <div
+        className="border-[1px] rounded-[2rem] lg:hidden flex justify-center items-center flex-col border-neutral-700 
+       hover:shadow-[0px_0px_5px_0px_#EDEDED] transition-all duration-300 w-full   col-span-1 row-span-1  bg-[#060606] "
+      >
+        <h1 className="text-lg  text-center font-oswald font-extrabold italic sm:text-xl ">
+          5
+        </h1>
+      </div>
+
+      {/* 
       <div
         className="border-[1px] rounded-[2rem] flex justify-center items-center flex-col border-neutral-700 
        hover:shadow-[0px_0px_5px_0px_#EDEDED] transition-all duration-300 w-full py-1 px-1 sm:py-2 sm:px-2  col-span-1 row-span-2 bg-[#060606] "
@@ -65,12 +105,14 @@ const MagicEnvision = () => {
           PlaceHolder
         </h1>
       </div>
-      <div className="border-[1px] rounded-[2rem] col-span-3 flex justify-center items-center flex-col border-neutral-700 
-       hover:shadow-[0px_0px_5px_0px_#EDEDED] transition-all duration-300 w-full py-1 px-1 sm:py-2 sm:px-2 bg-[#060606]">
+      <div
+        className="border-[1px] rounded-[2rem] col-span-3 flex justify-center items-center flex-col border-neutral-700 
+       hover:shadow-[0px_0px_5px_0px_#EDEDED] transition-all duration-300 w-full py-1 px-1 sm:py-2 sm:px-2 bg-[#060606]"
+      >
         <h1 className="text-lg  text-center font-oswald font-extrabold italic sm:text-xl  ">
           PlaceHolder
         </h1>
-      </div>
+      </div> */}
     </div>
   );
 };
