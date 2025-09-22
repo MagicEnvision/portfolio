0e07b55e70ac80d7848b52650fcfd71d9617ea01
magicenvision <rjrose2003@gmail.com>
2025-09-21 21:22:15 -0400

    added layout for projects

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
         <h1 className='text-xl sm:text-2xl lg:text-3xl xl:text-4xl font-oswald font-bold tracking-tighter mb-4'>
           All The Ways You Can Reach Me
         </h1>
-        <p className='text-sm lg:text-md xl:text-lg text-neutral-400'>
+        <p className='tracking-tighter text-sm lg:text-md xl:text-lg text-neutral-400'>
           Whether you're looking to collaborate, hire, or just say hello, I'd love to hear from you. My inbox is always open, and I'm active across various social platforms.
         </p>
       </div>
@@ -18,7 +18,7 @@ const Contact = () => {
         <h1 className='text-xl sm:text-2xl lg:text-3xl xl:text-4xl font-oswald font-bold tracking-tighter mb-4'>
           Location
         </h1>
-        <p className='text-sm lg:text-md xl:text-lg text-neutral-400'>
+        <p className='tracking-tighter text-sm lg:text-md xl:text-lg text-neutral-400'>
           Currently based in Toronto, Canada, but available for remote work globally. I'm always open to new opportunities and projects, no matter where you are.
         </p>
       </div>
@@ -28,7 +28,7 @@ const Contact = () => {
         <h1 className='text-xl sm:text-2xl lg:text-3xl xl:text-4xl font-oswald font-bold tracking-tighter mb-4'>
           Socials
         </h1>
-        <ul className='text-s lg:text-md  text-neutral-400 space-y-2'>
+        <ul className='tracking-tighter text-s lg:text-md  text-neutral-400 space-y-2'>
           <li><a href="https://github.com/MagicEnvision" className="hover:text-teal-500 text-sm lg:text-md xl:text-lg transition-colors">GitHub: /github/MagicEnvision</a></li>
           <li><a href="https://www.instagram.com/mr.magician_/" className="hover:text-pink-500 text-sm lg:text-md xl:text-lg  transition-colors">Instagram: /mr.magician_</a></li>
           <li><a href="https://x.com/magicenvision" className="hover:text-blue-500 text-sm lg:text-md xl:text-lg transition-colors">Twitter: /magicenvision</a></li>
@@ -40,7 +40,7 @@ const Contact = () => {
         <h1 className='text-xl sm:text-2xl lg:text-3xl xl:text-4xl font-oswald font-bold tracking-tighter mb-4'>
           Email and Phone Number
         </h1>
-        <p className='text-sm lg:text-md text-neutral-400'>
+        <p className='tracking-tighter text-sm lg:text-md text-neutral-400'>
           <span className="text-sm lg:text-md xl:text-lg block mb-2">Email: <a href="mailto:your.email@example.com" className="hover:text-orange-400 transition-colors">rjrose2003@gmail.com</a></span>
           <span className='text-sm lg:text-md xl:text-lg'>Phone: <a href="tel:+11234567890" className="hover:text-green-400 transition-colors">+1(647) 224-5530</a></span>
         </p>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,56 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>
-      Projects
+    <div className='flex flex-col justify-center items-center sm:grid sm:grid-cols-2 sm:grid-rows-2 gap-4 '>
+      <div className='border-[1px] h-[18rem] col-span-1 row-span-1 border-neutral-700 rounded-[2rem]  hover:shadow-[0px_0px_5px_0px_#EDEDED] transition-all duration-300
+       bg-[#060606] p-4 sm:p-8 justify-center flex'>
+        <div className='w-[30%] h-full'>
+          <h1 className='font-oswald text-4xl tracking-wide'>Title EX</h1>
+          <h2 className='font-oswald text-2xl tracking-tight'>Subtitle</h2>
+          <p className='text-lg tracking-tighter leading-5 text-neutral-400'>Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+
+        </div>
+        <div className='w-[70%] h-full'>
+
+        </div>
+      </div>
+      <div className='border-[1px] h-[18rem] col-span-1 row-span-1 border-neutral-700 rounded-[2rem]  hover:shadow-[0px_0px_5px_0px_#EDEDED] transition-all duration-300 bg-[#060606] p-4 sm:p-8 flex justify-center'>
+        <div className='w-[30%] h-full'>
+          <h1 className='font-oswald text-4xl tracking-wide'>Title EX</h1>
+          <h2 className='font-oswald text-2xl tracking-tight'>Subtitle</h2>
+          <p className='text-lg tracking-tighter leading-5 text-neutral-400'>Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+
+        </div>
+        <div className='w-[70%] h-full'>
+
+        </div>
+      </div>
+      
+      <div className='border-[1px] h-[18rem] col-span-1 row-span-1 border-neutral-700 rounded-[2rem]  hover:shadow-[0px_0px_5px_0px_#EDEDED] transition-all duration-300 bg-[#060606] p-4 sm:p-8 flex justify-center'>
+        <div className='w-[30%] h-full'>
+          <h1 className='font-oswald text-4xl tracking-wide'>Title EX</h1>
+          <h2 className='font-oswald text-2xl tracking-tight'>Subtitle</h2>
+          <p className='text-lg tracking-tighter leading-5 text-neutral-400'>Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+
+        </div>
+        <div className='w-[70%] h-full'>
+
+        </div>
+      </div>
+      
+      <div className='border-[1px] h-[18rem] col-span-1 row-span-1 border-neutral-700 rounded-[2rem]  hover:shadow-[0px_0px_5px_0px_#EDEDED] transition-all duration-300 bg-[#060606] p-4 sm:p-8 flex justify-center'>
+        <div className='w-[30%] h-full'>
+          <h1 className='font-oswald text-4xl tracking-wide'>Title EX</h1>
+          <h2 className='font-oswald text-2xl tracking-tight'>Subtitle</h2>
+          <p className='text-lg tracking-tighter leading-5 text-neutral-400'>Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+
+        </div>
+        <div className='w-[70%] h-full'>
+
+        </div>
+      </div>
+      
+      
     </div>
   )
 }
